5b69bfebd2572e3594657ac18c34aa989e6f8a0a
koray <347466123@qq.com>
2021-07-28 06:50:27 +0000

    1

---
+++ project/python/http.py
@@ -0,0 +1,6 @@
+
+# requests模块是HTTP请求库
+import requests as rq
+rq.Request()
+
+
---
+++ project/python/modul.py
@@ -0,0 +1,36 @@
+
+# 安装工具库
+# pip install XXX
+
+
+# Numpy是python在数据分析领域中不可或缺的工具,是pandas,scikit-learn等工具包的基础
+# 并提供了ndarray,matrix和ufunc
+
+import numpy as np
+# 创建数组
+np.array([2,5,7]) 
+np.zeros(2)
+np.ones(2)
+np.linspace(1, 2)
+
+
+
+# 数组常用操作
+
+np.reshape(a, newshape)
+
+
+# ufunc运算
+
+# random模块
+np.random.rand()
+np.randomr.randn()
+np.random.randint()
+
+
+
+
+
+
+
+
